dd942bfa3d7c3ba3fdf6187c14329c79bb55dd90
Harikrishnan1367709 <harikrishnan@mulecraft.in>
2024-12-31 18:42:22 +0530

    private router

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
-import { Link} from 'react-router-dom'
+import { Link, useNavigate} from 'react-router-dom'
 import "./Navbar.css"
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Navbar = () => {
     
 const user=useSelector((state)=>state.userinfo.Users)
+const dispatch=useDispatch();
+const navigate=useNavigate();
+const removeuser=()=>{
+  dispatch(deleteUser)
+  navigate("/")
+}
   return (
    <nav>
     <h1>Grony</h1>
     <ul>
+      {!user &&(
     <Link to="/">Login</Link>
+      )}
 
-     
-    
+     {user &&(
+    <>
      <Link to="/Users">Home</Link>
      <Link to="/About">Settings</Link>
-     <Link to="/Contact">Logout</Link>
+     <Link to="/Contact" onClick={removeuser}>Logout</Link>
+     </>
+     )}
      
     </ul>
    </nav>
